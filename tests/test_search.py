26b8289f5e3c6496a59d28fc2b83926230217d1f
dstilesr <d.stiles.r@hotmail.com>
2025-12-03 16:50:20 -0500

    Additional python tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -49,3 +49,25 @@ def test_search_simple():
     assert matches[5].value == "abc"
     assert matches[6].value == "bcd"
     assert matches[7].value == "cd"
+
+
+def test_search_multiple():
+    """
+    Test search with a simple dictionary over multiple texts
+    """
+    texts = ["abq cdr qpbcd 12abcd", "xy, tre, 1245, mllmkh, aqqsd", "432 bcda plodq"]
+    dct = ["ab", "abc", "cd", "bcd", "dq"]
+    matches: list[list[PyMatch]] = search_in_texts(dct, texts)
+
+    assert len(matches) == len(texts)
+    m1, m2, m3 = matches
+
+    assert len(m1) == 8
+    assert len(m2) == 0
+    assert len(m3) == 3
+
+    for match in m3:
+        assert texts[2][match.from_char : match.to_char] == match.value
+
+    for match in m1:
+        assert texts[0][match.from_char : match.to_char] == match.value
