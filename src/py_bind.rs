79582fddba0aed050680855345b88f710d527655
dstilesr <d.stiles.r@hotmail.com>
2025-12-03 14:47:04 -0500

    Some reorganization - add search_in_texts function

--- src/py_bind.rs
+++ src/py_bind.rs
@@ -21,7 +21,11 @@ fn map_error_py(err: SearchError) -> PyErr {
     }
 }
 
-/// Search for all occurences of strings in the "dictionary" in the given "haystack"
+/// Search for all occurences of strings in the "dictionary" in the given "haystack".
+///
+/// The dictionary must be a list of non-empty strings. It is usually better to process
+/// texts in batch if you are using the same dictionary, since this requires only one
+/// instantiation of the prefix tree.
 #[pyfunction]
 #[pyo3(signature = (dictionary: "list[str]", haystack: "str") -> "list[str]")]
 fn search_in_text(dictionary: Vec<String>, haystack: String) -> PyResult<Vec<String>> {
@@ -33,10 +37,29 @@ fn search_in_text(dictionary: Vec<String>, haystack: String) -> PyResult<Vec<Str
     Ok(matches.iter().map(|m| m.value().to_string()).collect())
 }
 
+/// Search for all occurences of strings in the "dictionary" in the given "haystack" strings.
+///
+/// The dictionary must be a list of non-empty strings. If you have multiple texts, this is more efficient
+/// than calling "search_in_text" individually on each one, since that would require the prefix tree to
+/// be instantiated multiple times.
+#[pyfunction]
+#[pyo3(signature = (dictionary: "list[str]", haystacks: "list[str]") -> "list[list[str]]")]
+fn search_in_texts(dictionary: Vec<String>, haystacks: Vec<String>) -> PyResult<Vec<Vec<String>>> {
+    let prefix_tree = create_prefix_tree(dictionary).map_err(map_error_py)?;
+    let mut matches_list = Vec::with_capacity(haystacks.len());
+
+    for h in &haystacks {
+        let matches = prefix_tree.find_text_matches(h).map_err(map_error_py)?;
+        matches_list.push(matches.iter().map(|m| m.value().to_string()).collect());
+    }
+
+    Ok(matches_list)
+}
+
 #[pyo3::pymodule]
 #[pyo3(name = "ah_search_rs")]
 pub mod aho_corasick_search {
 
     #[pymodule_export]
-    use super::search_in_text;
+    use super::{search_in_text, search_in_texts};
 }
